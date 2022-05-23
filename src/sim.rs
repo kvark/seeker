f2b174fa3a3e8034049332abaf91372d0ea8ff5e
Dzmitry Malyshau <kvark@fastmail.com>
2022-05-22 17:30:00 -0700

    Make simulation rules data-driven

--- src/sim.rs
+++ src/sim.rs
@@ -1,4 +1,5 @@
-use std::num::NonZeroU32;
+use rand::RngCore as _;
+use std::{collections::HashMap, num::NonZeroU32};
 
 use crate::grid::{Cell, Coordinate, Grid};
 
@@ -7,46 +8,72 @@ fn blend(new: f32, old: f32) -> f32 {
     new * BLEND_FACTOR + old * (1.0 - BLEND_FACTOR)
 }
 
-const NEIGHBORS: &[(Coordinate, Coordinate)] = &[
-    (-1, -1),
-    (-1, 0),
-    (-1, 1),
-    (0, -1),
-    (0, 1),
-    (1, -1),
-    (1, 0),
-    (1, 1),
-];
+type Weight = u32;
+type ProbabilityTable = HashMap<Weight, f32>;
+
+#[derive(Debug, Default, Eq, Hash, PartialEq)]
+struct Coordinates {
+    x: Coordinate,
+    y: Coordinate,
+}
+
+#[derive(Debug, Default)]
+struct Rules {
+    kernel: HashMap<Coordinates, Weight>,
+    spawn: ProbabilityTable,
+    keep: ProbabilityTable,
+}
+
+impl Rules {
+    fn new_conways_life() -> Self {
+        let mut rules = Self::default();
+        for x in [-1, 0, 1] {
+            for y in [-1, 0, 1] {
+                if x != 0 || y != 0 {
+                    rules.kernel.insert(Coordinates { x, y }, 1);
+                }
+            }
+        }
+        rules.spawn.insert(3, 1.0);
+        rules.keep.insert(2, 1.0);
+        rules.keep.insert(3, 1.0);
+        rules
+    }
+}
 
 pub struct Simulation {
     grids: [Grid; 2],
-    current_index: usize,
+    grid_index: usize,
+    rules: Rules,
+    rng: rand::rngs::ThreadRng,
 }
 
 impl Simulation {
     pub fn new(width: Coordinate, height: Coordinate) -> Self {
         Self {
             grids: [Grid::new(width, height), Grid::new(width, height)],
-            current_index: 0,
+            grid_index: 0,
+            rules: Rules::new_conways_life(),
+            rng: rand::thread_rng(),
         }
     }
 
     pub fn current(&self) -> &Grid {
-        self.grids.get(self.current_index).unwrap()
+        self.grids.get(self.grid_index).unwrap()
     }
 
     pub fn current_mut(&mut self) -> &mut Grid {
-        self.grids.get_mut(self.current_index).unwrap()
+        self.grids.get_mut(self.grid_index).unwrap()
     }
 
     pub fn advance(&mut self) {
-        let prev_index = self.current_index;
-        self.current_index = (self.current_index + 1) % self.grids.len();
+        let prev_index = self.grid_index;
+        self.grid_index = (self.grid_index + 1) % self.grids.len();
 
-        let (grids_before, grids_middle) = self.grids.split_at_mut(self.current_index);
+        let (grids_before, grids_middle) = self.grids.split_at_mut(self.grid_index);
         let (grids_current, grids_after) = grids_middle.split_at_mut(1);
         let cur = grids_current.first_mut().unwrap();
-        let prev = if prev_index < self.current_index {
+        let prev = if prev_index < self.grid_index {
             grids_before.first().unwrap()
         } else {
             grids_after.last().unwrap()
@@ -55,40 +82,55 @@ impl Simulation {
 
         for y in 0..size.1 {
             for x in 0..size.0 {
-                let neighbors_count = NEIGHBORS
+                let score = self
+                    .rules
+                    .kernel
                     .iter()
-                    .filter_map(|&(xa, ya)| prev.get(x + xa, y + ya))
-                    .count();
+                    .map(
+                        |(offsets, &weight)| match prev.get(x + offsets.x, y + offsets.y) {
+                            Some(_) => weight,
+                            None => 0,
+                        },
+                    )
+                    .sum::<Weight>();
+                let coin = (self.rng.next_u32() >> 8) as f32 / (1 << 24) as f32;
+
                 *cur.mutate(x, y) = match prev.get(x, y) {
-                    None if neighbors_count == 3 => {
-                        let mut avg_breed_age = 0.0;
-                        let mut avg_velocity = [0.0; 2];
-                        let denom = 1.0 / neighbors_count as f32;
-                        for &(xa, ya) in NEIGHBORS.iter() {
-                            if let Some(cell) = prev.get(x + xa, y + ya) {
-                                avg_breed_age +=
-                                    denom * blend(cell.age.get() as f32, cell.avg_breed_age);
-                                avg_velocity[0] += denom * blend(-xa as f32, cell.avg_velocity[0]);
-                                avg_velocity[1] += denom * blend(-ya as f32, cell.avg_velocity[1]);
+                    None => match self.rules.spawn.get(&score) {
+                        Some(&chance) if coin < chance => {
+                            let mut avg_breed_age = 0.0;
+                            let mut avg_velocity = [0.0; 2];
+                            for (offsets, &weight) in self.rules.kernel.iter() {
+                                if let Some(cell) = prev.get(x + offsets.x, y + offsets.y) {
+                                    let denom = weight as f32 / score as f32;
+                                    avg_breed_age +=
+                                        denom * blend(cell.age.get() as f32, cell.avg_breed_age);
+                                    avg_velocity[0] +=
+                                        denom * blend(-offsets.x as f32, cell.avg_velocity[0]);
+                                    avg_velocity[1] +=
+                                        denom * blend(-offsets.y as f32, cell.avg_velocity[1]);
+                                }
                             }
+                            Some(Cell {
+                                age: NonZeroU32::new(1).unwrap(),
+                                avg_breed_age,
+                                avg_velocity,
+                            })
                         }
-                        Some(Cell {
-                            age: NonZeroU32::new(1).unwrap(),
-                            avg_breed_age,
-                            avg_velocity,
-                        })
-                    }
-                    None => None,
-                    Some(cell) if neighbors_count >= 2 && neighbors_count <= 3 => Some(Cell {
-                        age: NonZeroU32::new(cell.age.get() + 1).unwrap(),
-                        avg_breed_age: cell.avg_breed_age,
-                        avg_velocity: [
-                            blend(0.0, cell.avg_velocity[0]),
-                            blend(0.0, cell.avg_velocity[1]),
-                        ],
-                    }),
-                    Some(_) => None,
-                }
+                        _ => None,
+                    },
+                    Some(cell) => match self.rules.keep.get(&score) {
+                        Some(&chance) if coin < chance => Some(Cell {
+                            age: NonZeroU32::new(cell.age.get() + 1).unwrap(),
+                            avg_breed_age: cell.avg_breed_age,
+                            avg_velocity: [
+                                blend(0.0, cell.avg_velocity[0]),
+                                blend(0.0, cell.avg_velocity[1]),
+                            ],
+                        }),
+                        _ => None,
+                    },
+                };
             }
         }
     }
