1b3e159e8539d7e31ebc736c47d640340e18ad48
Dzmitry Malyshau <kvark@fastmail.com>
2022-05-22 18:33:17 -0700

    Add user-facing RON rules representation

--- src/sim.rs
+++ src/sim.rs
@@ -9,7 +9,7 @@ fn blend(new: f32, old: f32) -> f32 {
 }
 
 type Weight = u32;
-type ProbabilityTable = HashMap<Weight, f32>;
+type Probability = f32;
 
 #[derive(Debug, Default, Eq, Hash, PartialEq)]
 struct Coordinates {
@@ -17,16 +17,20 @@ struct Coordinates {
     y: Coordinate,
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Rules {
     kernel: HashMap<Coordinates, Weight>,
-    spawn: ProbabilityTable,
-    keep: ProbabilityTable,
+    spawn: Vec<Probability>,
+    keep: Vec<Probability>,
 }
 
 impl Rules {
     fn new_conways_life() -> Self {
-        let mut rules = Self::default();
+        let mut rules = Self {
+            kernel: HashMap::default(),
+            spawn: vec![0.0; 10],
+            keep: vec![0.0; 10],
+        };
         for x in [-1, 0, 1] {
             for y in [-1, 0, 1] {
                 if x != 0 || y != 0 {
@@ -34,10 +38,94 @@ impl Rules {
                 }
             }
         }
-        rules.spawn.insert(3, 1.0);
-        rules.keep.insert(2, 1.0);
-        rules.keep.insert(3, 1.0);
+        rules.spawn[3] = 1.0;
+        rules.keep[2] = 1.0;
+        rules.keep[3] = 1.0;
         rules
+    }
+
+    fn get_sum(&self) -> Weight {
+        self.kernel.values().cloned().sum::<Weight>()
+    }
+
+    fn are_valid(&self) -> bool {
+        if self.kernel.get(&Coordinates::default()).is_some() {
+            return false;
+        }
+        let sum = self.get_sum() as usize;
+        if self.spawn.len() <= sum || !self.spawn.iter().all(|&prob| 0.0 <= prob && prob <= 1.0) {
+            return false;
+        }
+        if self.keep.len() <= sum || !self.keep.iter().all(|&prob| 0.0 <= prob && prob <= 1.0) {
+            return false;
+        }
+        true
+    }
+}
+
+type ProbabilityTable = HashMap<Weight, Probability>;
+
+#[derive(serde::Deserialize)]
+struct RulesConfig {
+    kernel: Vec<String>,
+    spawn: ProbabilityTable,
+    keep: ProbabilityTable,
+}
+
+#[derive(Debug)]
+enum RulesConfigError {
+    UnknownSymbol(char),
+    MissingKernelCenter,
+}
+
+impl RulesConfig {
+    fn parse(&self) -> Result<Rules, RulesConfigError> {
+        let mut rules = Rules {
+            kernel: HashMap::default(),
+            spawn: Vec::new(),
+            keep: Vec::new(),
+        };
+        let center = self
+            .kernel
+            .iter()
+            .enumerate()
+            .find_map(|(row, line)| {
+                line.chars()
+                    .position(|ch| ch == 'X')
+                    .map(|column| Coordinates {
+                        x: column as Coordinate,
+                        y: row as Coordinate,
+                    })
+            })
+            .ok_or(RulesConfigError::MissingKernelCenter)?;
+
+        for (row, line) in self.kernel.iter().enumerate() {
+            for (column, ch) in line.chars().enumerate() {
+                match ch {
+                    ' ' | 'X' => {}
+                    '0'..='9' => {
+                        let offset = Coordinates {
+                            x: column as Coordinate - center.x,
+                            y: row as Coordinate - center.y,
+                        };
+                        rules.kernel.insert(offset, ch as Weight - '0' as Weight);
+                    }
+                    _ => return Err(RulesConfigError::UnknownSymbol(ch)),
+                }
+            }
+        }
+
+        let sum = rules.get_sum() as usize;
+        rules.spawn.resize(sum + 1, 0.0);
+        rules.keep.resize(sum + 1, 0.0);
+
+        for (&weight, &prob) in self.spawn.iter() {
+            rules.spawn[weight as usize] = prob;
+        }
+        for (&weight, &prob) in self.keep.iter() {
+            rules.keep[weight as usize] = prob;
+        }
+        Ok(rules)
     }
 }
 
@@ -96,40 +184,35 @@ impl Simulation {
                 let coin = (self.rng.next_u32() >> 8) as f32 / (1 << 24) as f32;
 
                 *cur.mutate(x, y) = match prev.get(x, y) {
-                    None => match self.rules.spawn.get(&score) {
-                        Some(&chance) if coin < chance => {
-                            let mut avg_breed_age = 0.0;
-                            let mut avg_velocity = [0.0; 2];
-                            for (offsets, &weight) in self.rules.kernel.iter() {
-                                if let Some(cell) = prev.get(x + offsets.x, y + offsets.y) {
-                                    let denom = weight as f32 / score as f32;
-                                    avg_breed_age +=
-                                        denom * blend(cell.age.get() as f32, cell.avg_breed_age);
-                                    avg_velocity[0] +=
-                                        denom * blend(-offsets.x as f32, cell.avg_velocity[0]);
-                                    avg_velocity[1] +=
-                                        denom * blend(-offsets.y as f32, cell.avg_velocity[1]);
-                                }
+                    None if coin < self.rules.spawn[score as usize] => {
+                        let mut avg_breed_age = 0.0;
+                        let mut avg_velocity = [0.0; 2];
+                        for (offsets, &weight) in self.rules.kernel.iter() {
+                            if let Some(cell) = prev.get(x + offsets.x, y + offsets.y) {
+                                let denom = weight as f32 / score as f32;
+                                avg_breed_age +=
+                                    denom * blend(cell.age.get() as f32, cell.avg_breed_age);
+                                avg_velocity[0] +=
+                                    denom * blend(-offsets.x as f32, cell.avg_velocity[0]);
+                                avg_velocity[1] +=
+                                    denom * blend(-offsets.y as f32, cell.avg_velocity[1]);
                             }
-                            Some(Cell {
-                                age: NonZeroU32::new(1).unwrap(),
-                                avg_breed_age,
-                                avg_velocity,
-                            })
                         }
-                        _ => None,
-                    },
-                    Some(cell) => match self.rules.keep.get(&score) {
-                        Some(&chance) if coin < chance => Some(Cell {
-                            age: NonZeroU32::new(cell.age.get() + 1).unwrap(),
-                            avg_breed_age: cell.avg_breed_age,
-                            avg_velocity: [
-                                blend(0.0, cell.avg_velocity[0]),
-                                blend(0.0, cell.avg_velocity[1]),
-                            ],
-                        }),
-                        _ => None,
-                    },
+                        Some(Cell {
+                            age: NonZeroU32::new(1).unwrap(),
+                            avg_breed_age,
+                            avg_velocity,
+                        })
+                    }
+                    Some(cell) if coin < self.rules.keep[score as usize] => Some(Cell {
+                        age: NonZeroU32::new(cell.age.get() + 1).unwrap(),
+                        avg_breed_age: cell.avg_breed_age,
+                        avg_velocity: [
+                            blend(0.0, cell.avg_velocity[0]),
+                            blend(0.0, cell.avg_velocity[1]),
+                        ],
+                    }),
+                    _ => None,
                 };
             }
         }
